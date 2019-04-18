24b25f8557fce4501fa78f19861882c979be24c9
Kis Zoltán <kis.zoltan@servantes.hu>
2019-04-18 14:47:15 +0200

    broodcomb app

--- src/app/brood-comb/brood-comb.component.ts
+++ src/app/brood-comb/brood-comb.component.ts
@@ -5,43 +5,41 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './brood-comb.component.html',
   styleUrls: ['./brood-comb.component.css']
 })
-export class BroodCombComponent implements OnInit {
-	sizeVar:number = 50;
-	hexagon = (xCenter, yCenter, size) => {};
 
-	constructor() { }
+export class BroodCombComponent implements OnInit
+{
+	setSize:number;
+	private readonly context: CanvasRenderingContext2D;
+	numberOfSides:number = 6;
+	color:string = 'yellow';
 
-	broodComb(size)
+	constructor(private readonly canvas: HTMLCanvasElement) { 
+		this.context = this.canvas.getContext('2d');
+	}
+
+	public hexagon (xCenter, yCenter, size)
+	{ 
+		this.context.beginPath();
+		this.context.moveTo (xCenter +  size * Math.cos(0), yCenter +  size * Math.sin(0));          
+		for (let i = 1; i <= this.numberOfSides;i++) 
+		{
+			this.context.lineTo (xCenter + size * Math.cos(i * 2 * Math.PI / this.numberOfSides), yCenter + size * Math.sin(i * 2 * Math.PI / this.numberOfSides));
+		}
+		this.context.strokeStyle = this.color;
+		this.context.lineWidth = 0.5;
+		this.context.stroke();
+		return;
+	}
+
+	draw()
 	{
-		console.log("size: " + size);
-		size = this.sizeVar;
-		let canvas:any = document.getElementById('myCanvas');
-		let context = canvas.getContext('2d');
-		let color = "yellow";
-		let space= 3;
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-		let numberOfSides = 6;
-		let xCenter=canvas.width/2;
-		let yCenter=canvas.height/2;
+		let size = this.setSize;
+		this.canvas.width = window.innerWidth;
+		this.canvas.height = window.innerHeight;
+		let xCenter=this.canvas.width/2;
+		let yCenter=this.canvas.height/2;
 		let newX;
 		let newY;
-
-		// draw the hexagon
-		this.hexagon (xCenter, yCenter, size)
-		{ 
-			context.beginPath();
-			context.moveTo (xCenter +  size * Math.cos(0), yCenter +  size * Math.sin(0));          
-			for (let i = 1; i <= numberOfSides;i++) 
-			{
-				context.lineTo (xCenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), yCenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
-			}
-			context.strokeStyle = color;
-			console.log(context.strokeStyle);
-			context.lineWidth = 0.5;
-			context.stroke();
-			return;
-		}
 
 		//make the center hexagon:
 		this.hexagon(xCenter,yCenter,size);
@@ -56,19 +54,17 @@ export class BroodCombComponent implements OnInit {
 				this.hexagon(newX,newY,size)
 			}
 		}
-		
+
 		//make the filled hexagon:
 		xCenter = xCenter + Math.cos(11 * (2 * Math.PI / 12)) * (2 * size);
 		yCenter = yCenter - Math.sin(11 * (2 * Math.PI / 12)) * (2 * size);
 		this.hexagon(xCenter, yCenter ,size)
-		context.fillStyle = color;
-		context.fill();
-  	};
-
+		this.context.fillStyle = this.color;
+		this.context.fill();
+		console.log('x');
+	}
+	
 	ngOnInit() 
 	{
-		console.log( 'size: ' + this.sizeVar);
-		this.broodComb(this.sizeVar);
 	}
-
 }
